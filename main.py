6c31e43a456ebaaabb7d3e3e862093690e665ab0
yacinegithub64 <63664143+yacinegithub64@users.noreply.github.com>
2022-12-28 23:14:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,46 +6,38 @@ import boto3
 from filesystem import S3Filesystem
 
 def main():
-    # Read the config file
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description='SFTP-S3 bridge')
+    parser.add_argument('--config', required=True, help='Path to config file')
+    args = parser.parse_args()
+
+    # Load the config file
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(args.config)
 
-    # Parse the SFTP and logging configuration options
-    sftp_address = config['SFTP']['address']
-    sftp_port = int(config['SFTP']['port'])
-    sftp_username = config['SFTP']['username']
-    sftp_password = config['SFTP']['password']
-    error_log_file = config['Logging']['error_log']
-    error_log_enabled = config['Logging']['error_log_enabled'] == 'true'
-    access_log_file = config['Logging']['access_log']
-    access_log_enabled = config['Logging']['access_log_enabled'] == 'true']
+    # Get the SFTP and S3 config options
+    sftp_config = config['SFTP']
+    s3_config = config['S3']
 
-    # Create an S3 client
-    s3_address = config['S3']['address']
-    aws_access_key_id = config['S3']['access_key_id']
-    aws_secret_access_key = config['S3']['secret_access_key']
-    bucket = config['S3']['bucket']
-    s3_filesystem = S3Filesystem(s3_address, aws_access_key_id, aws_secret_access_key)
+    # Read the list of users and passwords from the config file
+    users = []
+    for section in config.sections():
+        if section.startswith('User '):
+            username = config[section]['username']
+            password = config[section]['password']
+            users.append((username, password))
 
-    # Start the SFTP server
-    server = paramiko.server.ServerInterface()
-    server.s3_filesystem = s3_filesystem
-    server.error_log_file = error_log_file
-    server.error_log_enabled = error_log_enabled
-    server.access_log_file = access_log_file
-    server.access_log_enabled = access_log_enabled
+    # Create the S3 filesystem
+    s3_filesystem = S3Filesystem(s3_config['aws_access_key_id'], s3_config['aws_secret_access_key'],
+                                 s3_config['region_name'], s3_config['bucket'])
 
-    try:
-        # Create an SFTP server and start listening for connections
-        ssh_server = paramiko.server.SSHServer()
-        ssh_server.add_global_request_handler(paramiko.SFTPServer.from_server, paramiko.SFTPServer)
-        ssh_server.start_server(server=server)
-    except Exception as e:
-        # Log the error and exit
-        if server.error_log_enabled:
-            with open(server.error_log_file, 'a') as f:
-                f.write(f'Error starting SFTP server: {e}\n')
-        return
+    # Create the SFTP server
+    server = SFTPServer(sftp_config['host_key'], sftp_config['host_key_alg'], sftp_config['listen_address'],
+                        sftp_config['listen_port'], sftp_config['error_log_file'], sftp_config['error_log_enabled'],
+                        sftp_config['access_log_file'], sftp_config['access_log_enabled'], s3_filesystem, users)
+
+    # Start the server
+    server.start()
 
 if __name__ == '__main__':
     main()
